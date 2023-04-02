c108a1ee35f1e444ef207d18735af7f7033dc66d
rroosimagi <rroosimagi@gmail.com>
2023-04-02 13:39:02 +0300

    Work

--- src/main/java/TempConvert/TemperatureConvertor.java
+++ src/main/java/TempConvert/TemperatureConvertor.java
@@ -1,18 +1,26 @@
 package TempConvert;
 
+import java.util.function.DoubleUnaryOperator;
+
 public class TemperatureConvertor {
-        private static final double ABSOLUTE_ZERO_CELSIUS = -273.0;
-        private static final double ABSOLUTE_ZERO_FAHRENHEIT = -459.0;
+    private static final double ABSOLUTE_ZERO_CELSIUS = -273.0;
+    private static final double ABSOLUTE_ZERO_FAHRENHEIT = -459.0;
 
-        public static double convertToCelsius(double fahrenheit) {
-            if (fahrenheit < ABSOLUTE_ZERO_FAHRENHEIT) {
-                throw new IllegalArgumentException("Temperature is below absolute zero.");
-            }return (fahrenheit - 32) / 1.8;
+    public static double convertToCelsius(double fahrenheit) {
+        if (fahrenheit < ABSOLUTE_ZERO_FAHRENHEIT) {
+            throw new IllegalArgumentException("Temperature is below absolute zero.");
         }
 
-        public static double convertToFahrenheit(double celsius) {
-            if (celsius < ABSOLUTE_ZERO_CELSIUS) {
-                throw new IllegalArgumentException("Temperature is below absolute zero.");
-            }return (celsius * 1.8) + 32;
-        }
+        DoubleUnaryOperator fahToCel = (fah) -> (fah - 32) / 1.8;
+        return fahToCel.applyAsDouble(fahrenheit);
     }
+
+    public static double convertToFahrenheit(double celsius) {
+        if (celsius < ABSOLUTE_ZERO_CELSIUS) {
+            throw new IllegalArgumentException("Temperature is below absolute zero.");
+        }
+
+        DoubleUnaryOperator celToFah = (cel) -> (cel * 1.8) + 32;
+        return celToFah.applyAsDouble(celsius);
+    }
+}
