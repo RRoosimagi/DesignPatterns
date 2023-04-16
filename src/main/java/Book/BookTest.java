b9300565c6567858391b417071ec8be811b52af5
rroosimagi <rroosimagi@gmail.com>
2023-04-16 10:55:57 +0300

    Work

--- src/main/java/Book/BookTest.java
+++ src/main/java/Book/BookTest.java
@@ -7,20 +7,12 @@ public class BookTest {
         Book book3 = new Book("Game of Thrones", " George R. R. Martin", 34);
         Book book4 = new Book("Head First Java", "Sierra & Bates", 65);
 
-        System.out.println("Book 1");
-        System.out.println("Author: " + book1.getAuthor());
-        System.out.println("Title: \"" + book1.getTitle() + "\"");
-        System.out.println("Price: " + book1.getPrice()+ " € ");
+        System.out.println("Book 1 " + book1.getAuthor()+ " \"" + book1.getTitle() + "\" " +book1.getPrice()+ " € ");
 
-        System.out.println("Book 2");
-        System.out.println("Author: " + book2.getAuthor());
-        System.out.println("Title: \"" + book2.getTitle() + "\"");
-        System.out.println("Price: " + book2.getPrice()+ " € ");
+        System.out.println("Book 2 " + book2.getAuthor()+ " \"" + book2.getTitle() + "\" " +book2.getPrice()+ " € ");
 
-        System.out.println("Book 3");
-        System.out.println("Author: " + book3.getAuthor());
-        System.out.println("Title: \"" + book3.getTitle() + "\"");
-        System.out.println("Price: " + book3.getPrice()+ " € ");
+
+        System.out.println("Book 3 " + book3.getAuthor()+ " \"" + book3.getTitle() + "\" " +book3.getPrice()+ " € ");
 
         System.out.println("Book 4");
         System.out.println("Author: " + book4.getAuthor());
